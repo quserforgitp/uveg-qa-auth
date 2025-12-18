3e31ca88324d557c47d674c1564bca174cda7ef8
quserforgitp <adealumnonegligas@gmail.com>
2025-12-17 18:57:12 -0600

    Quita imports no utilizados

--- src/test/java/org/uveg/Ambiente.java
+++ src/test/java/org/uveg/Ambiente.java
@@ -2,8 +2,8 @@ package org.uveg;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Ambiente {
 
